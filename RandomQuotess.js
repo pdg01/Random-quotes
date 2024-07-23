e21675a27ca4b7f00c1b81b83445a6c2e99f6f97
pdg01 <dennispenoiu19@gmail.com>
2024-07-23 15:21:48 +0300

    add project random quotes

--- RandomQuotess.js
+++ RandomQuotess.js
@@ -1,12 +1,20 @@
 var quotes = [
-    'Invata web development daca vrei bani multi',
-    'Web development-ul este de viitor',
-    'Poti lucra de oriunde',
-    'Treci dracului la treaba ca timpul trece',
-] 
+    '"Viitorul aparține celor care înțeleg tehnologia și o folosesc eficient." – John Doe',
+    'Într-o lume condusă de tehnologie, cei care o stăpânesc dețin cheia către oportunități nesfârșite." – Jane Smith',
+    'Industria IT nu este doar despre crearea de software; este despre modelarea viitorului." – Michael Johnson',
+    'Oportunitățile în IT sunt nelimitate pentru cei care sunt dispuși să învețe și să se adapteze." – Emily Davis',
+    'Inovația și tehnologia sunt pilonii gemeni ai pieței moderne a muncii, oferind nenumărate căi de creștere și succes." – Richard Williams',
+    'Abilitățile în tehnologie sunt moneda pieței muncii viitorului. Investește în ele cu înțelepciune." – Amanda Brown',
+    'Fiecare clic și fiecare linie de cod în IT deschide o nouă ușă către inovație și posibilități de angajare." – David Wilson',
+    'În era digitală, stăpânirea IT-ului este echivalentă cu a poseda o superputere care poate transforma industrii." – Susan Martinez',
+    'Sectorul IT este un teren de joacă pentru cei curioși și o mină de aur pentru cei determinați." – Robert Garcia',
+    'Frumusețea unei cariere în IT constă în evoluția sa constantă, prezentând noi provocări și oportunități în fiecare zi." – Patricia Rodriguez',
+
+]
 function newQuote()
     {
       var randomNumber = Math.floor(Math.random() * quotes.length);
-      document.getElementById('quoteDisplay').innerHTML = quotes[randomNumber];
+      document.getElementById('quoteDisplay').innerHTML = (randomNumber + 1) + ". " + quotes[randomNumber];
+        document.getElementById('random-number').innerHTML = randomNumber + 1;
     }
     
